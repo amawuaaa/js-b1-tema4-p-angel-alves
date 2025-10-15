f34954de44a46aea5efcde4cf1e78f56c662b581
Enric Vergara <donkikochan@gmail.com>
2025-10-15 09:13:52 +0200

    Initial commit

---
+++ src/U4EJ1.test.js
@@ -0,0 +1,28 @@
+import '@testing-library/jest-dom';
+import { tests, codeString } from '../U4/U4EJ1.js';
+describe("tests", () => {
+  const JSData = tests();
+  const myCodeString = codeString();
+  function getJSData() {
+    return new Promise((resolve, reject) => {
+      JSData.then((data) => {
+        resolve(data);
+      }).catch((error) => {
+        reject(error);
+      });
+    });
+  }
+  test('checks the function JSData returns JavaScript information from wikipedia', () => {
+    let myTest;
+    getJSData().then((data) => {
+      myTest=data;
+      expect(myCodeString.indexOf("await")).toBe(-1);
+      expect(myCodeString.indexOf("json")).toBeGreaterThan(-1);
+      expect(myCodeString.indexOf("then")).toBeGreaterThan(-1);
+      expect(myTest.indexOf('JavaScript')).toBeGreaterThan(-1);
+    }).catch((error) => {
+      myTest="error";
+      expect(myTest=="error").toBe(false);
+    });
+  });
+});
---
+++ src/U4EJ2.test.js
@@ -0,0 +1,29 @@
+import '@testing-library/jest-dom';
+import { tests, codeString } from '../U4/U4EJ2.js';
+describe("tests", () => {
+  const JSData = tests();
+  const myCodeString = codeString();
+  function getJSData() {
+    return new Promise((resolve, reject) => {
+      JSData.then((data) => {
+        resolve(data);
+      }).catch((error) => {
+        reject(error);
+      });
+    });
+  }
+  test('checks the function fetchOctocat returns user octocat information from GitHub', () => {
+    let myTest;
+    getJSData().then((data) => {
+      myTest=data;
+      expect(myCodeString.indexOf("await")).toBe(-1);
+      expect(myCodeString.indexOf("json")).toBeGreaterThan(-1);
+      expect(myCodeString.indexOf("then")).toBeGreaterThan(-1);
+      expect(typeof(myTest)).toBe('object');
+      expect(myTest.login).toBe('octocat');
+    }).catch((error) => {
+      myTest="error";
+      expect(myTest=="error").toBe(false);
+    });
+  });
+});
